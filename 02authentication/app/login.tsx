af99f59173a962b04a093cf5ae59841f0b0d1efe
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-04-20 16:44:47 +0530

    add useState hooks

--- 02authentication/app/login.tsx
+++ 02authentication/app/login.tsx
@@ -1,20 +1,21 @@
 import { View, Text, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Mybutton from '@/components/Mybutton'
 import { useRouter } from 'expo-router'
 
 export default function Login() {
-    const router = useRouter()
+  const [value1, setValue1] = useState("")
+  const [value2, setValue2] = useState("")
     const onLogin = () => {
-        router.navigate("/register")
+      console.log(value1, value2)
     }
     return (
         <View style={{ flex: 1, gap: 10 }}>
             <Image source={require("@/assets/images/login.png")} style={{ width: "100%", height: 400 }} resizeMode='cover' />
             <View style={{ gap: 20, paddingHorizontal:20}}>
-                <TextInput style={{ borderWidth: 1, height: 40, borderRadius:5 }} placeholder='  Enter Your Email'onChangeText={(e)=>(console.log(e))} />
+                <TextInput style={{ borderWidth: 1, height: 40, borderRadius:5 }} placeholder='  Enter Your Email'onChangeText={(e)=>(setValue1(e))} />
 
-                <TextInput style={{ borderWidth: 1, height: 40, borderRadius:5 }} placeholder='  Enter Your Password'onChangeText={(e)=>(console.log(e))} />
+                <TextInput style={{ borderWidth: 1, height: 40, borderRadius:5 }} placeholder='  Enter Your Password'onChangeText={(e)=>(setValue2(e))} />
                 <Mybutton title={"Login"} onPress={onLogin} />
             </View>
         </View>
