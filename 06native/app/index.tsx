b62042c18f109c6f451ee03484eb11fafc331c86
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-04-22 16:35:26 +0530

    Installed NativeWind and Tailwind dependencies

---
+++ 06native/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-black h-screen text-white flex items-center justify-center">
+      <Text className="text-white text-4xl font-extrabold">Namaskar Mandali</Text>
+      <Text className="text-white text-4xl font-extrabold">Hastay na Hasaylach pahije</Text>
+    </View>
+  );
+}
